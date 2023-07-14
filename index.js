56f741f63580129f763a6f4f305d09b7352b2101
Olga Blud <olga.blud@gmail.com>
2023-07-14 17:23:56 +0300

    Set date and time

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let city = "new-york";
+let city = "Netherlands";
 let apiKey = "1a87a8te48d8o34feb04cce044538ce4";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 // console.log(apiUrl);
@@ -8,10 +8,34 @@ function displayTemperature(response) {
   let weatherDescription = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   weatherDescription.innerHTML = response.data.condition.description;
   humidity.innerHTML = response.data.temperature.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = formatDate(response.data.time * 1000);
+}
+function formatDate(timestamp) {
+  let currentDate = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 axios.get(apiUrl).then(displayTemperature);
